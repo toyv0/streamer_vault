5ca41466d995722c14c7133abf719a8a787e71c7
toyvo <t0yv0@proton.me>
2022-04-23 09:29:02 +0200

    updating to support yeild strategy

--- src/Streamer.sol
+++ src/Streamer.sol
@@ -4,10 +4,22 @@ pragma solidity 0.8.10;
 import { SafeTransferLib } from "solmate/utils/SafeTransferLib.sol";
 import { ERC20 } from "solmate/tokens/ERC20.sol";
 import { ERC4626 } from "solmate/tokens/ERC4626.sol";
+import {Addresses} from "./utils/Addresses.sol";
 
 
 contract Streamer {
     using SafeTransferLib for ERC20;
+
+    /*//////////////////////////////////////////////////////////////
+                                 Public variables
+    //////////////////////////////////////////////////////////////*/ 
+
+    address public owner;
+    address public strategy;
+
+    /*//////////////////////////////////////////////////////////////
+                                 Structs 
+    //////////////////////////////////////////////////////////////*/ 
 
     // user account
     struct Account {
@@ -17,12 +29,22 @@ contract Streamer {
         // balance of base token
         uint256 balance;
 
+        // balance of strategy 
+        uint256 strategyBalance;
+
         // whitelisted addresses for an account (bool for active)
         // could create whitelist struct (allowance, num withdraws, etc)
         mapping(address => bool) whitelist;
     }
 
-    // events
+    struct Addresses {
+        address[] tokens;
+        mapping(address => uint256) indexes;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 Events
+    //////////////////////////////////////////////////////////////*/ 
 
     event Deposit(address indexed sender, address token, uint amount, uint balance);
     event DepositBase(address indexed sender, uint amount, uint balance);
@@ -32,22 +54,47 @@ contract Streamer {
 
     event AuthorizedAddress(address indexed accountOwner, address indexed newAddress, bool active);
 
-    // custom errors
+    event StrategyUpdated(address value);
+
+    /*//////////////////////////////////////////////////////////////
+                                 Custom Errors
+    //////////////////////////////////////////////////////////////*/ 
 
     error Unauthorized(address caller);
     error InsufficientBalance();
     error InvalidAccount();
     error ERC20CallFailed(address target, bool success, bytes data);
 
-
-    // private storage
+    /*//////////////////////////////////////////////////////////////
+                                 Private varialbes
+    //////////////////////////////////////////////////////////////*/ 
 
     // map of addresses and balances
     mapping(address => Account) private _accountOwners;
+    Addresses private _supportedStrategyTokens;
 
-    constructor() {}
+    constructor() {
+        strategy = Addresses.FUSE_POOL_18_ADDRESS;
+        owner = msg.sender;
+    }
 
-    // external functions
+    /*//////////////////////////////////////////////////////////////
+                                 External functions
+    //////////////////////////////////////////////////////////////*/ 
+
+    function getSupportedStrategyTokens() external view returns (address[] memory) {
+        return _supportedStrategyTokens.tokens;
+    }
+
+    function isSupportedStrategyToken(address token) external view returns (bool) {
+        return _strategyContains(token);
+    }
+
+    function setStrategy(address value) external view {
+        _onlyOwner();
+        strategy = value;
+        emit StrategyUpdated(value);
+    } 
 
     function authorizeAddress(address newAddress) external returns (address) {
         Account storage account = _accountOwners[msg.sender];
@@ -71,7 +118,9 @@ contract Streamer {
         return newAddress;
     }
 
-    // base token functions 
+    /*//////////////////////////////////////////////////////////////
+                                 Base token functions
+    //////////////////////////////////////////////////////////////*/ 
 
     receive() external payable {
         // get or create account
@@ -123,14 +172,58 @@ contract Streamer {
         return _balance;
     }
 
-    // ERC20 token functions
+     /*//////////////////////////////////////////////////////////////
+                                 Yeild Strategy Functions
+    //////////////////////////////////////////////////////////////*/
+
+    // how does the 4626 strategy know which token it is recieving and which token to return on withdraw
+
+    function depositToStrategy(address token, uint256 amount) external returns (uint256) {
+        // get or create account
+        Account storage account = _accountOwners[msg.sender];
+
+        if (_strategyContains(token)) {
+            token.approve(address(strategy), amount);
+            uint256 shares = ERC4626.deposit(amount, strategy); 
+
+            // update new balance 
+            uint256 newStrategyBalance = account.strategyBalance + shares;
+
+            // set balance of strategy 
+            account.strategyBalance = newStrategyBalance;
+        }
+
+        emit Deposit(msg.sender, token, amount, newStrategyBalance);
+
+        return newStrategyBalance;
+    }
+
+    function withdrawFromStrategy(address token, uint256 amount) external returns (uint256) {
+        _validStrategyWithdrawal(amount, msg.sender);
+
+        Account storage account = _accountOwners[msg.sender];
+
+        uint256 newStrategyBalance = account.strategyBalance - amount;
+
+        account.strategyBalance = newStrategyBalance;
+
+        ERC4626.withdraw(amount, msg.sender, strategy);
+
+        emit Withdraw(msg.sender, token, amount, newStrategyBalance);
+
+        return newStrategyBalance;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERC20 Token Functions
+    //////////////////////////////////////////////////////////////*/
 
     function deposit(address token, uint256 amount) external returns (uint256) {
         // get or create account
         Account storage account = _accountOwners[msg.sender];
 
         // update new balance 
-        uint256 balance = account.balances[token] + amount;
+        uint256 balance = account.balances[token] + shares;
 
         // set balance of token 
         account.balances[token] = balance;
@@ -184,12 +277,21 @@ contract Streamer {
         return balance;
     }
     
-    // validations
+    /*//////////////////////////////////////////////////////////////
+                                 VALIDATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    // check if the msg.sender is the owner
+    function _onlyOwner() internal view {
+        if (msg.sender != owner) {
+            revert Unauthorized(msg.sender);
+        }
+    }
 
     function _authorizedAccount(address accountOwner) internal view {
         Account storage account = _accountOwners[accountOwner];
 
-        // check if account is authorized
+        // check if account is authorized to withdraw from account owner
         if (account.whitelist[msg.sender] != true) {
             revert Unauthorized(msg.sender);
         }
@@ -198,7 +300,7 @@ contract Streamer {
     function _validWithdrawal(uint256 amount, address account, address token) internal view {
         Account storage _account = _accountOwners[account];
 
-        // check that the account has sufficient balance 
+        // check that the account has sufficient ERC20 balance 
         if (amount > _account.balances[token]) {
             revert InsufficientBalance();
         }
@@ -207,9 +309,35 @@ contract Streamer {
     function _validBaseWithdrawal(uint256 amount, address account) internal view {
         Account storage _account = _accountOwners[account];
 
-        // check that the account has sufficient base balance 
+        // check that the account has sufficient base token balance 
         if (amount > _account.balance) {
             revert InsufficientBalance();
         }
+    }
+
+    function _validStrategyWithdrawal(uint256 amount, address account) internal view {
+        Account storage _account = _accountOwners[account];
+
+        if (amount > _account.strategyBalance) {
+            revert InsufficientBalance();
+        }
+    }
+
+    // check that token is supported by the current strategy 
+    function _strategyContains(address token) internal view returns (bool) {
+        return _supportedStrategyTokens.indexes[token] != 0;
+    }
+
+    // only a owner can add a token to the strategy 
+    function _addTokenToStrategy(address token) internal returns (bool) {
+        _onlyOwner();
+
+        if (_strategyContains(token)) {
+            return false;
+        }
+        
+        _supportedStrategyTokens.tokens.push(token);
+        _supportedStrategyTokens.indexes[token] = _supportedStrategyTokens.tokens.length;
+        return true;
     }
 }
---
+++ src/utils/Addresses.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.8.0;
+
+library Addresses {
+    address constant FUSE_POOL_18_ADDRESS = 
+        0x17b1A2E012cC4C31f83B90FF11d3942857664efc;
+}
